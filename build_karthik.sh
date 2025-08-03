99eb01626ba1ad004dd70676941d733a2bc37e75
Karthik Nair <realkarthiknair@gmail.com>
2025-08-04 03:28:57 +0530

    copy unmasked resume to $HOME/RESUME

--- build_karthik.sh
+++ build_karthik.sh
@@ -1,4 +1,5 @@
 ./build.sh -i docs/resume_karthik.md -o docs/KarthikNair.pdf --sanitize false
+cp docs/KarthikNair.pdf $HOME/RESUME/KarthikNair.pdf
 ./io_push.sh #personal script to push unmasked resume to somewhere
 ./src/sanitize.py -i docs/resume_karthik.md -o docs/resume.md
 ./build.sh -i docs/resume_karthik.md -o docs/KarthikNair_sanitized.pdf --sanitize true
